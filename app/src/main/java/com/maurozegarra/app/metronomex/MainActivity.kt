bfe3f6b0eb564afa55b4d1ae957b854c2920c524
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-06-24 16:26:30 -0500

    Release a simple metronome engine working

---
+++ app/src/main/java/com/maurozegarra/app/metronomex/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.maurozegarra.app.metronomex
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val KEY_INPUT = "key_input"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var editTextInput: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editTextInput = findViewById(R.id.edit_text_input)
+    }
+
+    fun startService(view: View) {
+        val input = editTextInput.text.toString()
+
+        val serviceIntent = Intent(this, MetronomeService::class.java)
+        serviceIntent.putExtra(KEY_INPUT, input)
+
+        startService(serviceIntent)
+    }
+
+    fun pauseService(view: View) {
+        val serviceIntent = Intent(this, MetronomeService::class.java)
+        stopService(serviceIntent)
+    }
+}
---
+++ app/src/main/java/com/maurozegarra/app/metronomex/MetronomeService.kt
@@ -0,0 +1,74 @@
+package com.maurozegarra.app.metronomex
+
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.media.AudioAttributes
+import android.media.SoundPool
+import android.os.Handler
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+// This is the number of milliseconds in a minute
+const val ONE_MINUTE = 60_000L
+
+// This is the number of beats per minute
+const val BPM = 100
+
+class MetronomeService : Service() {
+    private var handler = Handler()
+    private lateinit var runnable: Runnable
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val input = intent.getStringExtra(KEY_INPUT)
+
+        val notificationIntent = Intent(this, MainActivity::class.java)
+
+        val pendingIntent = PendingIntent.getService(this, 0, notificationIntent, 0)
+        val notification =
+            NotificationCompat.Builder(this, App.CHANNEL_ID)
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText(input) // do the actual work
+                .setSmallIcon(R.drawable.ic_metronome)
+                .setContentIntent(pendingIntent)
+                .build()
+
+        startForeground(1, notification)
+
+        /* do heavy work on a background thread */
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+
+        val soundPool = SoundPool.Builder()
+            .setMaxStreams(1)
+            .setAudioAttributes(audioAttributes)
+            .build()
+
+        val tick = soundPool.load(this, R.raw.tick, 1)
+
+        val interval = getInterval()
+
+        runnable = Runnable {
+            soundPool.play(tick, 1.0F, 1.0F, 0, 0, 1.0F)
+            handler.postDelayed(runnable, interval)
+        }
+
+        handler.postDelayed(runnable, interval)
+
+        // stopSelf();
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacks(runnable)
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    private fun getInterval() = ONE_MINUTE / BPM
+}
