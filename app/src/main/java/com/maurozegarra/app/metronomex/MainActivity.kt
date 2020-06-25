282501446a62771686210a4f071b30b8a0281096
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-06-25 12:25:05 -0500

    Update to only one toggle button
    Previously a start and stop button were used

--- app/src/main/java/com/maurozegarra/app/metronomex/MainActivity.kt
+++ app/src/main/java/com/maurozegarra/app/metronomex/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
@@ -13,25 +14,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var editTextInput: EditText
+    private lateinit var buttonToggle: Button
+    private var eventPlay = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         editTextInput = findViewById(R.id.edit_text_input)
+        buttonToggle = findViewById(R.id.button_toggle)
     }
 
-    fun startService(view: View) {
+    fun toggleService(view: View) {
+        // toggle previous state
+        eventPlay = !eventPlay
+
+        if (eventPlay) startService() else pauseService()
+    }
+
+    private fun startService() {
         val input = editTextInput.text.toString()
 
         val serviceIntent = Intent(this, MetronomeService::class.java)
         serviceIntent.putExtra(KEY_INPUT, input)
 
         startService(serviceIntent)
+        buttonToggle.text = getString(R.string.pause)
     }
 
-    fun pauseService(view: View) {
+    private fun pauseService() {
         val serviceIntent = Intent(this, MetronomeService::class.java)
         stopService(serviceIntent)
+        buttonToggle.text = getString(R.string.start)
     }
 }
